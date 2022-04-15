5c67a495539db90284623916cd3c30be9f0d5c16
ShapeShifter420 <55488794+ShapeShifter420@users.noreply.github.com>
2022-04-15 11:22:51 +0500

    errors and make file

--- first_task/first.c
+++ first_task/first.c
@@ -1,22 +1,34 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 int main(int argc, char **argv){
 int BLOCK_SIZE = 4096;    
 char block[BLOCK_SIZE];
 int fd1 = 0;
-int fd2 = open(argv[1],O_WRONLY | O_CREAT,775);
+int fd2 = 1;
 if (argc > 2){
 	fd1 = open(argv[1],O_RDONLY);
 	fd2 = open(argv[2],O_WRONLY | O_CREAT,775);
+
 	if (argc == 4)
 		BLOCK_SIZE = atoi(argv[3]);
 	}
+    else fd2 = open(argv[1],O_WRONLY | O_CREAT,775);
+    if (errno)
+        {printf("Ошибка открытия файла");
+        return 1;}
 while(read(fd1,block,BLOCK_SIZE)){
+    if (errno)
+        {printf("Ошибка чтения файла");
+        return 1;}
     if (check_zero_block(block,BLOCK_SIZE)){
         lseek(fd2,BLOCK_SIZE,SEEK_CUR);}
-    else
-        write(fd2,block,BLOCK_SIZE);}
+    else{
+        if (errno)
+        {printf("Ошибка записи в файл");
+        return 1;}
+        write(fd2,block,BLOCK_SIZE);}}
 close(fd2);
 
 }
